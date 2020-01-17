399fd3b437535ffba445075bde5ad340d5303f5c
ShrutiRawal <shruti456rawal@gmail.com>
2020-01-17 16:17:43 +0530

    Notification

--- app/src/main/java/com/example/codeutsava/HomeActivity.java
+++ app/src/main/java/com/example/codeutsava/HomeActivity.java
@@ -1,19 +1,60 @@
 package com.example.codeutsava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setupViewPager();
+
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel =
+                    new NotificationChannel("notif","notif", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        FirebaseMessaging.getInstance().subscribeToTopic("general")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Succesful";
+                        if (!task.isSuccessful()) {
+                            msg = "Failed to receive notification";
+                        }
+                        Toast.makeText(HomeActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 
     private void setupViewPager() {
@@ -30,5 +71,20 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_iot);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_petrol_pump);
 
+    }
+
+    public void button(View view) {
+        notification.setSmallIcon(R.drawable.ic_petrol_pump);
+        notification.setTicker("Petrol Alert");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Petrol level low");
+        notification.setContentText("Refill tank soon to avoid inconvinience");
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID,notification.build());
     }
 }
